1cf42ee1961b5c9fc362e23f0d80a83ea3288283
sleepanda <jusuck4523@gmail.com>
2023-12-14 21:11:56 +0900

    feat: add cogs

--- main.py
+++ main.py
@@ -6,21 +6,26 @@ from discord.ext import commands
 load_dotenv()
 
 # bot init 
+bot_token = os.getenv('BOT_TOKEN')
 intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
-game = discord.Game("!사용방법") 
-bot = commands.Bot(command_prefix='!',activity=game, intents=intents)
+game = discord.Game("/help") 
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+# get Cogs dir
+cogs_path = 'Cogs'
+abs_cogs_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), cogs_path)
+
+# load Cogs
+for ext in os.listdir(abs_cogs_path):
+    if ext.endswith(".py"):
+        bot.load_extension(f"Cogs.{ext.split('.')[0]}")
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    await bot.change_presence(status=discord.Status.online, activity=game,)
+    print("Bot is ready")
 
 def main():
-    bot_token = os.environ['BOT_TOKEN']
     bot.run(bot_token)
     return
 
