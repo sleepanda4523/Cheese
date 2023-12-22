59dc30135b283759229adb99f4702c4a34fd2925
sleepanda <jusuck4523@gmail.com>
2023-12-22 20:10:42 +0900

    feat: change nickname

--- Cogs/user.py
+++ Cogs/user.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import pandas as pd
 import sqlite3
 import os
+import asyncio
 
 # sqlite func
 def connect(filename = './db/utopia.db'):
@@ -98,6 +99,56 @@ class User(commands.Cog):
             
         db_close(con, cur)
         return
+        
+    @commands.command(name='닉변')
+    async def change_name(self, ctx, name:discord.Member=None, new_name:str=None):
+         #chack input
+        if name == None or new_name == None:
+            embed = discord.Embed(title="경고", description="인자 값이 부족합니다. \n `$닉변 [현재 닉네임] [바꿀 닉네임]` 형태로 입력해주세요.", color=0xFE2E2E)
+            return await ctx.send(embed=embed)
+        
+        memberlist = [member.nick if member.nick else member.name for member in ctx.guild.members]
+        if new_name in memberlist:
+            embed = discord.Embed(title="확인", description="중복되는 닉네임이 존재합니다. 그래도 변경하시나요?\n (아래 이모지를 선택해주세요!)", color=0xFACC2E)
+            check_msg = await ctx.send(embed=embed)
+            await check_msg.add_reaction("⭕")
+            await check_msg.add_reaction("❌")
+            
+            def check(reaction, user):
+                print(user, ctx.message)
+                return user == ctx.message.author and reaction.message.channel == ctx.message.channel and reaction.message.id == check_msg.id
+            
+            try:
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=60.0, check=check)
+            except asyncio.TimeoutError:
+                embed = discord.Embed(title="에러", description=f"시간 초과. {ctx.author}님 다시 시도해주세요.", color=0xFE2E2E)
+                return await ctx.send(embed=embed)
+            else:
+                if str(reaction.emoji) == '⭕':
+                    userid = name.id
+                    con, cur = connect()
+                    sql = f'UPDATE {self.table_name} SET name="{new_name}" WHERE userid = {userid}'
+                    cur.execute(sql)
+                    con.commit()
+                    db_close(con, cur)
+                    await check_msg.clear_reactions()
+                    await name.edit(nick=new_name)
+                    embed = discord.Embed(title="이름 변경 완료", description=f"{name.mention}로 이름 변경 되셨습니다!", color=0x00FFFF)
+                    return await check_msg.edit(embed=embed)
+                elif str(reaction.emoji) == '❌':
+                    await check_msg.clear_reactions()
+                    embed = discord.Embed(title="이름 미변경", description=f"따로 이름을 변경하지 않으셨어요..!", color=0x00FFFF)
+                    return await check_msg.edit(embed=embed)
+        else:
+            await name.edit(nick=new_name)
+            userid = name.id
+            con, cur = connect()
+            sql = f'UPDATE {self.table_name} SET name="{new_name}" WHERE userid = {userid}'
+            cur.execute(sql)
+            con.commit()
+            db_close(con, cur)
+            embed = discord.Embed(title="이름 변경 완료", description=f"{name.mention}로 이름 변경 되셨습니다!", color=0x00FFFF)
+            return await ctx.send(embed=embed)
         
     @role.error
     async def role_handler(self, ctx, error):
