c67e20822a361a6be78fd21649db7687b60d5607
sleepanda <jusuck4523@gmail.com>
2023-12-22 21:28:23 +0900

    feat: add attendance

--- Cogs/Work.py
+++ Cogs/Work.py
@@ -4,6 +4,7 @@ import pandas as pd
 import sqlite3
 import os
 import asyncio
+import datetime
 
 # sqlite func
 def connect(filename = './db/utopia.db'):
@@ -35,7 +36,7 @@ def create_table_user(conn, cur, tablename='works'):
         sql = "PRAGMA foreign_keys"
         cur.execute(sql)
         conn.commit()
-        sql = f'CREATE TABLE {tablename} (userid INTEGER NOT NULL, name TEXT NOT NULL, count INTEGER, first_work TEXT, last_work TEXT, last_quit, constraint work_FK FOREIGN KEY (userid, name) REFERENCES users(userid, name) )'
+        sql = f'CREATE TABLE {tablename} (userid INTEGER NOT NULL, name TEXT NOT NULL, count INTEGER, check_a INTEGER, first_work TEXT, last_work TEXT, last_quit TEXT, constraint work_FK FOREIGN KEY (userid, name) REFERENCES users(userid, name) )'
         cur.execute(sql)
         conn.commit()
 
@@ -47,6 +48,55 @@ class Work(commands.Cog):
         create_table_user(conn, cur, self.table_name)
         self.df = change_df(conn, self.table_name)
         db_close(conn, cur)
+
+    @commands.command(name='출석')
+    async def attendance(self, ctx, name:discord.Member=None):
+        if name is None:
+            name = ctx.author
+
+        userid = name.id
+        nickname = name.nick if name.nick else name.name
+        conn, cur = connect()
+        sql = f'SELECT EXISTS ( SELECT * FROM users WHERE userid = {userid} );'
+        cur.execute(sql)
+        if cur.fetchall()[0][0] == 0:
+            embed = discord.Embed(title="에러", description="등록된 유저가 없습니다. \n `$역할` 명령어를 사용해주세요!", color=0xFE2E2E)
+            return await ctx.send(embed=embed)
+        else:
+            # name update
+            sql = f'SELECT name FROM users WHERE userid = {userid}'
+            cur.execute(sql)
+            if str(cur.fetchall()[0][0]) != nickname:
+                sql = f'UPDATE users SET name="{nickname}" WHERE userid = {userid}'
+                cur.execute(sql)
+                conn.commit()
+            # check works
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            sql = f'SELECT EXISTS ( SELECT * FROM {self.table_name} WHERE userid = {userid} );'
+            cur.execute(sql)
+            count = -1
+            if cur.fetchall()[0][0] == 0:
+                cur.execute(f'INSERT INTO {self.table_name} VALUES(?,?,?,?,?,?,NULL);', (userid, nickname, 1, 1, now, now))
+                conn.commit()
+                count = 1
+            else :
+                sql = f'SELECT check_a, count FROM {self.table_name} WHERE userid = {userid}'
+                cur.execute(sql)
+                result = cur.fetchall()[0]
+                chk = result[0]
+                count = result[1]
+                if chk == 1:
+                    embed = discord.Embed(title="확인", description="저번 출근 이후 퇴근을 안눌렀어요. \n 다음에는 꼭 퇴근도 눌러주세요.", color=0xFACC2E)
+                    msg = await ctx.send(embed=embed)
+                    await asyncio.sleep(3)
+                sql = f'UPDATE {self.table_name} SET check_a=1, count={count+1}, last_work="{now}" WHERE userid = {userid}'
+                cur.execute(sql)
+                conn.commit()
+                embed = discord.Embed(title="출석 완료", description=f"{name.mention} 출석 완료. \n 현재 출석 횟수 : {count+1} ", color=0x00FFFF)
+                return await msg.edit(embed=embed)
+            embed = discord.Embed(title="출석 완료", description=f"{name.mention} 출석 완료. \n 현재 출석 횟수 : {count} ", color=0x00FFFF)
+            return await ctx.send(embed=embed)
+                
         
 async def setup(bot):
     await bot.add_cog(Work(bot))
